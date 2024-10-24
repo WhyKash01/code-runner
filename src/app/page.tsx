9120fece8ce2259a2ce533330d630f63cfbb6581
WhyKash01 <itsyash9211@gmail.com>
2024-10-25 01:20:54 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client"
+import Loader from "@/components/Loader";
 import Heropage from "./../components/Heropage"
+import { Loading } from "@/Store/atom";
+import { useRecoilState } from "recoil";
 export default function Home() {
-  
+  const [loading, setLoading] = useRecoilState(Loading);
   return (
     <div>
-    <div className="">
+    <div className=" flex justify-center items-center">
       <Heropage></Heropage>
+      <div className="absolute z-50 ">{loading?<Loader/>:<></>}</div>
     </div>
     </div>
   );
--- src/components/JoinRoom.tsx
+++ src/components/JoinRoom.tsx
@@ -13,19 +13,28 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { room } from "@/Store/atom";
-
+import { Loading, room } from "@/Store/atom";
+import { RocketIcon } from "@radix-ui/react-icons"
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
 import { useRecoilState } from "recoil";
 import { useSocket } from "./SocketComponent";
 
 export default function AlertDialogDemo() {
   const { socket, isConnected } = useSocket();
+  const [al,setAl]=useState(false)
   const [Room, setRoom] = useRecoilState(room);
   const router:any = useRouter(); // Get router from Next.js
+  const [loading, setLoading] = useRecoilState(Loading);
   console.log(router)
   const handleJoinRoom = () => {
     console.log(router)
-    if (Room) {
+    try {
+      setLoading(true)
+      if (Room) {
         setRoom(Room)
         console.log("Joining room:", Room); // Debug room value before emitting
         socket.emit("join-room", Room);
@@ -38,9 +47,32 @@ export default function AlertDialogDemo() {
     } else {
       console.error("Room is undefined or empty");
     }
+    } catch (error) {
+      console.log(error)
+    }
+    finally{
+      setTimeout(() => {
+        setLoading(false)
+        setAl(true)
+      }, 5000);
+    }
   };
-
+  if(al){
+    setTimeout(()=>{
+      setAl(false)
+    },5000)
+  }
   return (
+    <div className="flex justify-center items-center">
+      
+    {al?<Alert className="absolute w-[40vw] left-[30vw] top-[10vh] border border-red-900">
+      <RocketIcon className="h-10 w-10 mt-2" />
+      <AlertTitle className="text-red-600 text-xl ml-7">ConnectionError</AlertTitle>
+      <AlertDescription className="text-lg text-red-600 ml-7">
+      Error: Unable to connect to the room. Please try again.
+      </AlertDescription>
+    </Alert>:<></>}
+      
     <AlertDialog>
       <AlertDialogTrigger asChild>
         <Button className="text-white bg-zinc-950 hover:bg-zinc-800 focus:outline-none focus:ring-4 focus:ring-sky-200 font-medium rounded-full text-lg px-10 py-6 text-center me-2 mb-2">
@@ -62,7 +94,7 @@ export default function AlertDialogDemo() {
               console.log("Room value set to:", e.target.value); // Debug input change
             }}
             placeholder="Room Name"
-          />
+            />
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
@@ -75,5 +107,6 @@ export default function AlertDialogDemo() {
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
+    </div>
   );
 }
